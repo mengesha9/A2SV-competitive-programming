55210b9b8c039244c665e0c2ccbef073cb4f3754
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2021-12-18 23:19:46 -0800

    Create FizzBuzz.py

---
+++ weak1/FizzBuzz.py
@@ -0,0 +1,11 @@
+n = int(input("enter N: "))
+fizbuzz = []
+for i in range(1,n+1):
+    if x%15 == 0:
+        fizbuzz.append("FizzBuzz")
+    elif x % 5 == 0:
+        fizbuzz.append("Buzz")
+    elif x % 3 == 0:
+        fizbuzz.append(Fizz)
+return fizbuzz
+    
