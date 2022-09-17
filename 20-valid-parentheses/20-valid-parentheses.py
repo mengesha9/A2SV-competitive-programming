36a15b671fae1c2fe25fcd78a21ba6a0d10bdf08
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-09-17 05:03:50 -0700

    Time: 56 ms (36.45%), Space: 13.9 MB (72.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,15 +1,28 @@
 class Solution:
+    # def isValid(self, s):
+    #     stack=[]
+    #     for i in range(len(s)):
+    #         if s[i]=='(' or s[i]=='[' or s[i]=='{' :
+    #             stack.append(s[i])
+    #         elif stack :
+    #             if stack[-1]==']' or stack[-1]==')' or stack[-1]=='}':
+    #                 stack.pop()
+    #             return stack
+    #         else:
+    #             return False
+    #     return stack==[]
+
     def isValid(self,s):
-        stack = []
-        close_parentheses = {')': '(', '}': '{', ']': '['}
+        stack=[]
+        hash_close={')':'(','}':'{',']':'['}
         for p in s:
-            if p in close_parentheses.values():
+            if p in hash_close.values():
                 stack.append(p)
-            elif stack and close_parentheses[p] == stack[-1]:
-                stack.pop()
+            elif stack and stack[-1]==hash_close[p]:
+                stack.pop()               
             else:
                 return False
-        return stack == [] 
+        return stack==[]
 
             
             
