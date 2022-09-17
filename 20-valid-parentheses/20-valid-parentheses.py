d1205d1b5ee5cef52d74ea4f5ec82ad77e18e1ae
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-09-17 06:09:53 -0700

    Time: 53 ms (44.59%), Space: 13.9 MB (72.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -8,31 +8,7 @@ class Solution:
             elif stack==[] or bracket != hashMa_close[stack.pop()]:
                 return False
         return stack == []    
-    # def isValid(self, s):
-    #     stack=[]
-    #     for i in range(len(s)):
-    #         if s[i]=='(' or s[i]=='[' or s[i]=='{' :
-    #             stack.append(s[i])
-    #         elif stack :
-    #             if stack[-1]==']' or stack[-1]==')' or stack[-1]=='}':
-    #                 stack.pop()
-    #             return stack
-    #         else:
-    #             return False
-    #     return stack==[]
-
-    # def isValid(self,s):
-    #     stack=[]
-    #     hash_close={')':'(','}':'{',']':'['}
-    #     for p in s:
-    #         if p in hash_close.values():
-    #             stack.append(p)
-    #         elif stack and hash_close[p]==stack[-1]:
-    #             stack.pop()               
-    #         else:
-    #             return False
-    #     return stack==[]
-
+   
             
             
 
