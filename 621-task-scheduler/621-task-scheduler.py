a6ebed8689e3247868363ec665f5a2bd9787067a
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-09-25 04:05:41 -0700

    Update 621-task-scheduler.py

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -10,11 +10,7 @@ class Solution(object):
         max_number = max(lst)
         counter=lst.count(max_number)
         
-#         i=0
-#         counter = 0
-#         while i < len(lst) and lst[i] == max_number:
-#             counter += 1
-#             i +=1
+
             
         ret = (max_number -1)* (n+1) + counter
         return max(ret,len(tasks))
