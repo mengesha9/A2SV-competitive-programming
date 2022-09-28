7057e0913d8fb7d18d296282c8a759acabd21b76
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-09-28 03:24:25 -0700

    Create 1877_Minimize_Maximum_Pair_Sum_in_Array.py

---
+++ 1877_Minimize_Maximum_Pair_Sum_in_Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        i=0
+        j=len(nums)-1
+        arr=[]
+        while i<j:
+            arr.append(nums[i]+nums[j])
+            i+=1
+            j-=1
+        return max(arr)    
+ 
