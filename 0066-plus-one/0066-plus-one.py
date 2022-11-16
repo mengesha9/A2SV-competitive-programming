0af9ea59c3b1305546e39d9e82abc3187eaf6331
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-11-16 13:26:54 +0300

    Time: 39 ms (86.25%), Space: 13.9 MB (59.80%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,18 +2,33 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
         
-        x=''.join(map(str,digits))
-        # print(x)   
-        y=int(x)
-        y+=1
-        # print(y)
+#         x=''.join(map(str,digits))
+#         # print(x)   
+#         y=int(x)
+#         y+=1
+#         # print(y)
         
-        y=str(y)
+#         y=str(y)
         
-        # print(y)
-        arr=[]
-        for i in range(len(y)):
-            arr.append(y[i])
-        return arr    
+#         # print(y)
+#         arr=[]
+#         for i in range(len(y)):
+#             arr.append(y[i])
+#         return arr    
         
+        digits=digits[::-1] 
+        one=1
+        i=0
+        while one:
+            if i<len(digits):
+                if digits[i]==9:
+                    digits[i]=0
+                else:
+                    digits[i]+=1
+                    one=0
+            else:
+                digits.append(1)
+                one=0
+            i+=1    
+        return   digits[::-1]       
         
