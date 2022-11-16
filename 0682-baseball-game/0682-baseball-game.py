ad680a62fafefc4b7173d8a59014d4d4d4936f01
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-11-16 15:42:26 +0300

    Time: 89 ms (30.30%), Space: 14.2 MB (6.46%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -7,22 +7,23 @@ class Solution:
             if operations[i]=='C':
                 arr.pop()
             elif operations[i]=="D":
-                y=arr.pop()
-                x=y*2
-                arr.append(y)
-                arr.append(x)
+                arr.append(2*arr[-1])
+                # y=arr.pop()
+                # x=y*2
+                # arr.append(y)
+                # arr.append(x)
             elif  operations[i]=="+":
-                y=arr.pop()
-                x=arr.pop()
-                arr.append(x)
-                arr.append(y)
-                arr.append(x+y)
+                
+                arr.append(arr[-1]+arr[-2])
+                
+                # y=arr.pop()
+                # x=arr.pop()
+                # arr.append(x)
+                # arr.append(y)
+                # arr.append(x+y)
             else:
                 arr.append(int(operations[i]))
-        count=0       
-        for i in range(len(arr)):
-            count+=arr[i]
-        return count    
+        return sum(arr) 
             
                 
                 
