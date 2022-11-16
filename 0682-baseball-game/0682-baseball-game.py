7fb97d812a84eb08269941438876fc81ae10229b
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2022-11-16 15:29:38 +0300

    Time: 83 ms (47.66%), Space: 14.1 MB (32.03%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,34 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+        
+        arr=[]
+        for i in range(len(operations)):
+            if operations[i]=='C':
+                arr.pop()
+            elif operations[i]=="D":
+                y=arr.pop()
+                x=y*2
+                arr.append(y)
+                arr.append(x)
+            elif  operations[i]=="+":
+                y=arr.pop()
+                x=arr.pop()
+                arr.append(x)
+                arr.append(y)
+                arr.append(x+y)
+            else:
+                arr.append(int(operations[i]))
+        return sum(arr)       
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
