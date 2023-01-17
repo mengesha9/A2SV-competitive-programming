4f704c79db769235cf97cec4fb93ac0fb386a7d7
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2023-01-17 04:13:43 -0800

    Create 2007_Find_Original_Array_From_Doubled_Array.py

---
+++ 2007_Find_Original_Array_From_Doubled_Array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed.sort()
+        if len(changed)%2!=0:
+            return []
+        count=Counter(changed)
+        print(count)
+        # {1: 1, 3: 1, 4: 1, 2: 1, 6: 1, 8: 1}
+        # [1,3,4,2,6,8]
+        # ({6: 1, 3: 1, 0: 1, 1: 1})   [6,3,0,1]
+        arr=[]
+        for i in range(len(changed)):
+            if changed[i] in count and count[changed[i]]>=1 :
+                count[changed[i]]-=1
+                if changed[i]*2 in count and count[changed[i]*2]>=1:
+
+                    arr.append(changed[i])
+                    count[2*changed[i]]-=1
+                # if len(arr) ==len(changed)//2:
+                #     return arr   
+
+        return  arr if len(arr)==len(changed)//2 else []    
