32d8bd533bf4b115fd7f7ec86cc27704497366d6
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2023-01-21 05:17:31 -0800

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+#!/bin/python3
+
 import math
 import os
 import random
@@ -14,22 +16,18 @@ import sys
 
 def countSwaps(a):
     # Write your code here
-    numSwaps = 0
-    # firstElement=0
-    # lastElement=0
+    count=0
     for i in range(len(a)):
-        for j in range(len(a)-i-1):
-            if a[j] > a[j+1] :
-                (a[j],a[j+1]) =(a[j+1] , a[j])
-                numSwaps +=1
-            firstElement = a[0]
-            lastElement= a[len(a)-1]
-    print("Array is sorted in " + str(numSwaps)+" swaps.")
-    print("First Element: " + str(firstElement))
-    print("Last Element: " + str(lastElement))
-        
+        for j in range(1,len(a)):
+            if a[j]<a[j-1]:
+                a[j],a[j-1]=a[j-1],a[j]
+                count+=1
+    print("Array is sorted in "+str(count)+" swaps.")             
+    print("First Element: "+str(a[0]))
+    print("Last Element: "+str(a[-1]))            
                 
-                
+            
+
 
 if __name__ == '__main__':
     n = int(input().strip())
