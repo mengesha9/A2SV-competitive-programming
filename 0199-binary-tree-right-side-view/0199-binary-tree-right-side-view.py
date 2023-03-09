056e78f70cfbca2ea54e600345161bddb0effdd8
mengesha9 <95374219+mengesha9@users.noreply.github.com>
2023-03-09 00:54:44 -0800

    Time: 36 ms (50.76%), Space: 13.9 MB (60.09%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,87 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+ 
+        if not root:
+            return []
+        length=self.depth(root)
+        print(length)
+
+        stack=[]
+        stack.append([root])  
+        res = []
+        res.append(root.val)
+        while length >= 0 :
+            node = stack.pop()
+            arr = []
+            temp = []
+            for n in node:
+                if n.left:
+                    arr.append(n.left.val)
+                    temp.append(n.left)
+                if n.right:
+                    arr.append(n.right.val)
+                    temp.append(n.right)
+            if arr:        
+                res.append(arr[-1])
+                
+            stack.append(temp)
+
+            length -= 1
+        # print(res)
+        return res  
+    def depth(self, root):
+
+        if not root:
+            return 0
+        left = self.depth(root.left)
+        right = self.depth(root.right)
+
+        return max(left, right) + 1    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         arr=[]
+#         node=root
+#         while node :
+#             arr.append(node.val)
+#             node=node.right
+#         print(arr)
+        
+#         return arr
+        
+
+        
