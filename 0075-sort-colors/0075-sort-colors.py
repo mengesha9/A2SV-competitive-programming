b467a7261cfe9e356679163e918161a75d6c124d
Mengistu Ashebir <95374219+mengesha9@users.noreply.github.com>
2023-03-24 09:27:42 -0700

    Time: 37 ms (52.90%), Space: 13.9 MB (51.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        for i in range(len(nums)):
+            cur_min=i
+            for j in range(i,len(nums)):
+                if nums[j]<nums[cur_min]:
+                    cur_min=j
+            nums[cur_min] ,nums[i]=nums[i],nums[cur_min]
+        
+        
