794f7316ac8119006d468d15226b8c6b3dde84d4
Mengistu Ashebir <95374219+mengesha9@users.noreply.github.com>
2023-09-20 00:37:14 -0700

    added 2-keys-keyboard.python

---
+++ 2-keys-keyboard.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+             
+        anser = 1  
+        arr = [] 
+        d = 2  
+        while d <=n :
+            while n % d == 0:
+                n /= d
+                arr.append(d)
+            d += 1
+
+        return sum(arr)
