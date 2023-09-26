d58757fa7f6d876be30bff9924aee63811222fa2
Mengistu Ashebir <95374219+mengesha9@users.noreply.github.com>
2023-09-26 14:28:28 -0700

    added last-stone-weight-ii.python

--- last-stone-weight-ii.py
+++ last-stone-weight-ii.py
@@ -1,18 +1,32 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
 
-        total = sum(stones)
-        dp = {}
-        
-        def dfs(ind,currsum):
-            if currsum >= ceil(total/2) or ind >= len(stones):
-                return abs(currsum-(total-currsum))
-           
-            if (ind,currsum) not in dp:
-                dp[(ind,currsum)] = min( dfs(ind+1,currsum) , dfs(ind+1,currsum+stones[ind]))
+        visited = set()
+        def dfs(ind,sum):
+            if ind >= len(stones):
+                return 0
+            take = stones[ind] + sum
+            nottake = sum
+
+            visited.add(take)    
+            visited.add(nottake)
+
+            dfs(ind+1,take)
+            dfs(ind+1,nottake)
             
-            return dp[(ind,currsum)]
+            return 
 
-          
+        for k in visited:
+            print(k,end="")   
 
-        return dfs(0,0)
+        dfs(0,0)
+        total = sum(stones)
+        num = (total//2)+1
+        ans = float("Inf")
+        while num >=0 :
+            if total - num in visited:
+                ans = min(ans,abs(total-2*num))
+
+            num-=1
+
+        return ans
