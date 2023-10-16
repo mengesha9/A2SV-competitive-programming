56a21071f72c9b88c674565a64179f817bccc7ad
Mengistu Ashebir <95374219+mengesha9@users.noreply.github.com>
2023-10-16 09:51:34 +0300

    Update unique-paths.py

--- unique-paths.py
+++ unique-paths.py
@@ -1,16 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        hashmap = defaultdict(int)
-        def back(r,c):
-            if min(r,c) < 0 or r>= m or c >= n:
-                return 0
-            if r == m-1 and c == n-1:
-                return 1
+        grid = [[1]*n for i in range(m)]
 
-            if (r,c) not in hashmap:
-                hashmap[(r,c)]  = back(r+1,c) + back(r,c+1)
-
-            return hashmap[(r,c)]
-
-        return back(0,0)
+        for i in range(1,m):
+            for j in range(1,n):
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        return grid[m-1][n-1]        
+        
